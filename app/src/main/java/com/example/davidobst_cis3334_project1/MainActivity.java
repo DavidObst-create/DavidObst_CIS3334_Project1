47d3f5cf8e4e0b1dc4f61571faa0fb8d5abbe09d
jobst <jobst@css.edu>
2023-10-19 11:24:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/davidobst_cis3334_project1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.davidobst_cis3334_project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
